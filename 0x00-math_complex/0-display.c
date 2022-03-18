81cde3bf3f7df16801901e8facb17c29865eed3e
Masindi Cindy <59694595+MasindiCindi12@users.noreply.github.com>
2022-03-18 11:14:26 +0200

    Update 0-display.c

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -1,32 +1,29 @@
 #include "main.h"
 
 /**
-* display_complex_number - Display complex number
-* @c: Complex number
-*/
+ * display_complex_number - Prints the representation of a complex number
+ *
+ * @c: complex number
+ * Return: Void
+ */
 
 void display_complex_number(complex c)
 {
-	if (c.re == 0)
+  printf("%.9g", c.re);
+  if (c.im)
+    {
+      if (c.im > 0)
 	{
-		if (c.im < 0)
-			printf("- %.9gi\n", c.im * (-1));
-		else if (c.im == 0)
-			printf("0\n");
-		else
-			printf("%.9gi\n", c.im);
+	  printf(" + ");
 	}
-	else
+      else
 	{
-		if (c.im < -1)
-			printf("%.9g - %.9gi\n", c.re, c.im * (-1));
-		else if (c.im == -1)
-			printf("%.9g - i\n", c.re);
-		else if (c.im == 0)
-			printf("%.9g\n", c.re);
-		else if (c.im == 1)
-			printf("%.9g + i\n", c.re);
-		else
-			printf("%.9g + %.9gi\n", c.re, c.im);
+	  printf(" - ");
+	  c.im *= -1;
 	}
+      if (c.im != 1 && c.im != -1)
+	printf("%.9g", c.im);
+      printf("i");
+    }
+  printf("\n");
 }
